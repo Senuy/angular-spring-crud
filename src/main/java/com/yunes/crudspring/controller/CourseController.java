8825744bd1a1066d818b54bb9989d16a7f0a4337
Yunes da Silva Santos <yunes.santos@PMCG.IMTI>
2024-06-12 08:22:37 -0400

    alterações até aula 39

--- src/main/java/com/yunes/crudspring/controller/CourseController.java
+++ src/main/java/com/yunes/crudspring/controller/CourseController.java
@@ -3,8 +3,8 @@ package com.yunes.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,30 +13,35 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.yunes.crudspring.model.Course;
 import com.yunes.crudspring.repository.CourseRepository;
+import com.yunes.crudspring.service.CourseService;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
-
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService){
+        this.courseService = courseService;
+    }
 
     @GetMapping
     public @ResponseBody List<Course> list() {
-        return courseRepository.findAll();
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
-        return courseRepository.findById(id)
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
+        return courseService.findById(id)
                 .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
@@ -44,33 +49,26 @@ public class CourseController {
     // @RequestMapping(method=RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course) {
+    public Course create(@RequestBody @Valid Course course) {
         // System.out.println(course.getName());
         // return
         // ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
-        return courseRepository.save(course);
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id,
-            @RequestBody Course course) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
+    public ResponseEntity<Course> update(@PathVariable  @NotNull @Positive Long id,
+            @RequestBody @Valid Course course) {
+        return courseService.update(id, course)
+                .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
-        return courseRepository.findById(id)
-        .map(recordFound -> {
-            courseRepository.deleteById(id);
+    public ResponseEntity<Void> delete(@PathVariable  @NotNull @Positive Long id){
+        if (courseService.delete(id)){
             return ResponseEntity.noContent().<Void>build();
-        })
-        .orElse(ResponseEntity.notFound().build());
+    }
+        return   ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/yunes/crudspring/model/Course.java
+++ src/main/java/com/yunes/crudspring/model/Course.java
@@ -1,4 +1,6 @@
 package com.yunes.crudspring.model;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -7,12 +9,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
 @Data
 @Entity
-//@Table(name = )
+//@Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@SQLRestriction("status <> 'Inativo'")
 public class Course {
 
     @Id
@@ -20,11 +28,24 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200,nullable = false)
+    @NotBlank
+    @NotNull
+    @Size( min = 5, max = 100)
+    @Column(length = 100,nullable = false)
     private String name;
 
+    @NotNull
+    @Size(max = 10)
+    @Pattern(regexp = "Back-End|Front-End")
     @Column(length = 10,nullable = false)
     private String category;
+
+
+    @NotNull
+    @Size(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10,nullable = false)
+    private String status = "Ativo";
 
 
 }
