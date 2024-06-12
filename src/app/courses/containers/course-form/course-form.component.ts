38b07df34499295bc34bbfb451cd41ad2a707a63
Yunes da Silva Santos <yunes.santos@PMCG.IMTI>
2024-06-12 08:24:49 -0400

    Alterações até aula 39

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -3,7 +3,7 @@ import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { AppMaterialModule } from '../../../shared/app-material/app-material.module';
 import { CoursesService } from '../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Location } from '@angular/common';
+import { Location,CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../../model/course';
 
@@ -11,7 +11,7 @@ import { Course } from '../../model/course';
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [AppMaterialModule],
+  imports: [AppMaterialModule,CommonModule],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss',
 })
@@ -64,23 +64,24 @@ export class CourseFormComponent {
   private onError() {
     this.snackBar.open('Erro ao Salvar Curso', '', { duration: 2000 });
   }
-  getErrorMessage(fieldName: string) {
-    const field = this.form.get(fieldName);
+  getErrorMessage( fieldName: string ) {
+    const field = this.form.get( fieldName );
 
-    if (field?.hasError('required')) {
-      return 'Campo obrigatório';
+    const erros = this.form.getError( fieldName );
+
+    if ( field?.hasError( 'required' ) ) {
+      return 'Campo Obrigatório';
+    }
+    if ( field?.hasError( 'minlength' ) ) {
+      const requiredLength = erros ? erros[ 'requiredlength' ] : 5;
+      return `Tamanho MÍNIMO precisa ser de ${requiredLength} caracteres`;
     }
 
-    if (field?.hasError('minlength')) {
-      const requiredLength: number = field.errors ? field.errors['minlength']['requiredLength'] : 5;
-      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    if ( field?.hasError( 'maxlength' ) ) {
+      const requiredLength = erros ? erros[ 'requiredlength' ] : 50;
+      return `Tamanho MÁXIMO é de ${requiredLength} caracteres`;
     }
 
-    if (field?.hasError('maxlength')) {
-      const requiredLength: number = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
-      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
-    }
-
-    return 'Campo Inválido';
+    return 'Campo Inválido.';
   }
 }
