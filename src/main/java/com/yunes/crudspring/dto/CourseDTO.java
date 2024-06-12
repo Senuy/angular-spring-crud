78e6e33d5ef812ac6cb9e410e44e1d738b0baa28
Yunes da Silva Santos <yunes.santos@PMCG.IMTI>
2024-06-12 10:13:09 -0400

    DTO

--- src/main/java/com/yunes/crudspring/dto/CourseDTO.java
+++ src/main/java/com/yunes/crudspring/dto/CourseDTO.java
@@ -1,4 +1,14 @@
 package com.yunes.crudspring.dto;
 
-public record CourseDTO() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CourseDTO(
+    @JsonProperty("_id") Long id,
+    @NotBlank @NotNull @Size( min = 5, max = 100)String name,
+    @NotNull @Size(max = 10) @Pattern(regexp = "Back-End|Front-End") String category) {
 }
